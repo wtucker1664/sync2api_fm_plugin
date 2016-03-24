7ec938b685b61474c91833fa5815097b01e5d5a8
Warren Tucker <wtucker1664@me.com>
2016-03-24 17:43:37 +0000

    Windows plugin done

---
+++ Support/FMTemplate/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FMTemplate.rc
+//
+#define PLUGIN_VERSION_MINOR            0
+#define PLUGIN_VERSION_BUILD            0
+#define PLUGIN_VERSION_DEV              0
+#define WANT_IDLE                       0
+#define WANT_PREFERENCES                0
+#define PLUGIN_NAME_ID                  1
+#define PLUGIN_VERSION_MAJOR            1
+#define PLUGIN_PREFERENCES_ID           2
+#define PLUGIN_VERSION_ID               3
+#define FUNCTION_OFFSET                 100
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
